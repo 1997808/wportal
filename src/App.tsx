f01b5f16d6c97da13f35365c077b90d36bee77ce
Le Duy Khanh <khanhld13@vpbank.com.vn>
2024-09-25 15:19:43 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Background from './assets/windows.jpg'
+
+const App = () => {
+  return (
+    <div className='min-w-screen w-full max-h-screen h-screen'>
+      <img src={Background} className='w-full h-screen object-cover' />
+
+    </div>
+  )
+}
+
+export default App
