5a9ce7e5d4f0086bd11c751d8490562361c97d05
Le Duy Khanh <khanhld13@vpbank.com.vn>
2024-09-30 11:25:46 +0700

    User icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,65 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { forwardRef, useEffect, useRef, useState } from 'react';
 import Background from './assets/windows.jpg'
+import UserIcon from './assets/excited.webp'
 import RGL, { WidthProvider } from "react-grid-layout";
-import { motion, useTransform, useScroll } from "framer-motion"
+import { motion } from "framer-motion"
 
 const cardData = [
   { key: 'a', x: 0, y: 0, w: 2, h: 2, color: 'bg-red-600', static: true },   // 2x2 block
   { key: 'k', x: 6, y: 0, w: 1, h: 1, color: 'bg-cyan-600', static: true },   // 1x1 block
   { key: 'l', x: 7, y: 0, w: 1, h: 1, color: 'bg-amber-600', static: true },  // 1x1 block
   { key: 'b', x: 6, y: 1, w: 2, h: 2, color: 'bg-blue-600', static: true },  // 2x2 block
-  { key: 'c', x: 2, y: 0, w: 1, h: 1, color: 'bg-yellow-600', static: true }, // 1x1 block
-  { key: 'd', x: 3, y: 0, w: 1, h: 1, color: 'bg-green-600', static: true },  // 1x1 block
-  { key: 'e', x: 4, y: 0, w: 2, h: 1, color: 'bg-purple-600', static: true }, // 2x1 block
+  { key: 'c', x: 4, y: 0, w: 1, h: 1, color: 'bg-yellow-600', static: true }, // 1x1 block
+  { key: 'd', x: 5, y: 0, w: 1, h: 1, color: 'bg-green-600', static: true },  // 1x1 block
+  { key: 'e', x: 2, y: 0, w: 2, h: 1, color: 'bg-blue-600', static: true }, // 2x1 block
   { key: 'e2', x: 2, y: 1, w: 2, h: 1, color: 'bg-purple-600', static: true }, // 2x1 block
   { key: 'c2', x: 4, y: 1, w: 1, h: 1, color: 'bg-yellow-600', static: true }, // 1x1 block
   { key: 'd2', x: 5, y: 1, w: 1, h: 1, color: 'bg-green-600', static: true },  // 1x1 block
-  { key: 'f', x: 0, y: 2, w: 1, h: 1, color: 'bg-pink-600', static: true },   // 1x1 block
-  { key: 'g', x: 1, y: 2, w: 1, h: 1, color: 'bg-orange-600', static: true }, // 1x1 block
+  { key: 'f', x: 0, y: 2, w: 2, h: 1, color: 'bg-pink-600', static: true },   // 1x1 block
   { key: 'h', x: 2, y: 2, w: 2, h: 1, color: 'bg-teal-600', static: true },   // 2x1 block
   { key: 'i', x: 4, y: 2, w: 1, h: 1, color: 'bg-indigo-600', static: true }, // 1x1 block
   { key: 'j', x: 5, y: 2, w: 1, h: 1, color: 'bg-lime-600', static: true },   // 1x1 block
   { key: 'm', x: 0, y: 3, w: 2, h: 1, color: 'bg-rose-600', static: true },   // 2x1 block
-  { key: 'n', x: 2, y: 3, w: 1, h: 1, color: 'bg-fuchsia-600', static: true }, // 1x1 block
-  { key: 'o', x: 3, y: 3, w: 1, h: 1, color: 'bg-blue-600', static: true },   // 1x1 block
+  { key: 'n', x: 2, y: 3, w: 2, h: 1, color: 'bg-fuchsia-600', static: true }, // 1x1 block
   { key: 'p', x: 4, y: 3, w: 1, h: 1, color: 'bg-red-600', static: true },    // 1x1 block
   { key: 'q', x: 5, y: 3, w: 2, h: 1, color: 'bg-yellow-600', static: true }, // 2x1 block
   { key: 'r', x: 7, y: 3, w: 1, h: 1, color: 'bg-green-600', static: true },    // 1x1 block
 ];
 
+const cardData2 = [
+  { key: 't1', x: 0, y: 0, w: 2, h: 2, color: 'bg-red-600', static: true },   // 2x2 block
+  { key: 't2', x: 6, y: 0, w: 1, h: 1, color: 'bg-cyan-600', static: true },   // 1x1 block
+  { key: 't3', x: 7, y: 0, w: 1, h: 1, color: 'bg-amber-600', static: true },  // 1x1 block
+  { key: 't4', x: 6, y: 1, w: 2, h: 2, color: 'bg-blue-600', static: true },  // 2x2 block
+  { key: 't5', x: 2, y: 0, w: 1, h: 1, color: 'bg-yellow-600', static: true }, // 1x1 block
+  { key: 't6', x: 3, y: 0, w: 1, h: 1, color: 'bg-green-600', static: true },  // 1x1 block
+  { key: 't7', x: 4, y: 0, w: 2, h: 1, color: 'bg-purple-600', static: true }, // 2x1 block
+  { key: 't8', x: 2, y: 1, w: 2, h: 1, color: 'bg-purple-600', static: true }, // 2x1 block
+  { key: 't9', x: 4, y: 1, w: 1, h: 1, color: 'bg-yellow-600', static: true }, // 1x1 block
+  { key: 't10', x: 5, y: 1, w: 1, h: 1, color: 'bg-green-600', static: true },  // 1x1 block
+  { key: 't11', x: 0, y: 2, w: 1, h: 1, color: 'bg-pink-600', static: true },   // 1x1 block
+  { key: 't12', x: 1, y: 2, w: 1, h: 1, color: 'bg-orange-600', static: true }, // 1x1 block
+  { key: 't13', x: 2, y: 2, w: 2, h: 1, color: 'bg-teal-600', static: true },   // 2x1 block
+  { key: 't14', x: 4, y: 2, w: 1, h: 1, color: 'bg-indigo-600', static: true }, // 1x1 block
+  { key: 't15', x: 5, y: 2, w: 1, h: 1, color: 'bg-lime-600', static: true },   // 1x1 block
+  { key: 't16', x: 0, y: 3, w: 2, h: 1, color: 'bg-rose-600', static: true },   // 2x1 block
+  { key: 't17', x: 2, y: 3, w: 1, h: 1, color: 'bg-fuchsia-600', static: true }, // 1x1 block
+  { key: 't18', x: 3, y: 3, w: 1, h: 1, color: 'bg-blue-600', static: true },   // 1x1 block
+  { key: 't19', x: 4, y: 3, w: 1, h: 1, color: 'bg-red-600', static: true },    // 1x1 block
+  { key: 't20', x: 5, y: 3, w: 2, h: 1, color: 'bg-yellow-600', static: true }, // 2x1 block
+  { key: 't21', x: 7, y: 3, w: 1, h: 1, color: 'bg-green-600', static: true },    // 1x1 block
+];
+
 const GridLayout = WidthProvider(RGL);
 
-
 const App = () => {
-  // const { height, width } = useWindowDimensions();
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [layout, setLayout] = useState(cardData.map((item) => {
+    const { key, x, y, w, h, static: staticMode } = item
+    return { i: key, x, y, w, h, static: staticMode }
+  }));
+  const [layout2, setLayout2] = useState(cardData2.map((item) => {
     const { key, x, y, w, h, static: staticMode } = item
     return { i: key, x, y, w, h, static: staticMode }
   }));
@@ -108,16 +133,23 @@ const App = () => {
     <div className='relative min-w-screen w-full max-h-screen h-screen'>
       <img src={Background} className='w-full h-screen object-cover' />
       <div className='absolute top-0 left-0 max-h-screen h-screen pt-16 pb-24 flex flex-col gap-24'>
-        <div className='px-48'>
+        <div className='px-48 flex justify-between items-center'>
           <h1 className='text-7xl text-white font-light'>Start</h1>
+          <div className='flex gap-4 items-center px-2 py-1 hover:bg-gray-500/50 bg-transparent duration-150 cursor-pointer' onClick={() => console.log('test')}>
+            <div className='flex flex-col items-end gap-1'>
+              <p className='text-3xl text-white font-light'>John Doe</p>
+              <p className='text-lg text-white font-light'>Developer</p>
+            </div>
+            <img src={UserIcon} className='w-16 h-16' />
+          </div>
         </div>
         <section className='relative grid-container grow flex gap-16 w-screen overflow-x-scroll [&>*]:shrink-0' ref={gridRef}>
           <div className='px-16'></div>
           {dimensions.height !== 0 && dimensions.width !== 0 && (
-            <GridItem height={dimensions.height} width={dimensions.width} layout={layout} />
+            <GridItem height={dimensions.height} width={dimensions.width} layout={layout} items={cardData} />
           )}
           {dimensions.height !== 0 && dimensions.width !== 0 && (
-            <GridItem height={dimensions.height} width={dimensions.width} layout={layout} />
+            <GridItem height={dimensions.height} width={dimensions.width} layout={layout2} items={cardData2} />
           )}
           <div className='px-16'></div>
         </section>
@@ -127,8 +159,8 @@ const App = () => {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const GridItem = (props: { height: number, width: number, layout: any }) => {
-  const { height, width, layout } = props //from grid container
+const GridItem = (props: { height: number, width: number, layout: any, items: any }) => {
+  const { height, width, layout, items } = props //from grid container
   const gridWidth = height * 2
 
   return gridWidth && (
@@ -145,7 +177,7 @@ const GridItem = (props: { height: number, width: number, layout: any }) => {
       isBounded={true}
       style={{ width: `${gridWidth}px` }}
     >
-      {cardData.map((card) => {
+      {items.map((card: never) => {
         return (
           // @ts-expect-error xdd
           <WCard key={card.key} keyId={card.key} color={card.color} />
